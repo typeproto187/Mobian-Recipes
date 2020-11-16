9cfda719d890a1adbc34e8396062bc4c13ea1aac
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-11-16 17:23:12 +0100

    sunxi: bootloader.sh: adjust comment

--- devices/sunxi/bootloader.sh
+++ devices/sunxi/bootloader.sh
@@ -9,7 +9,7 @@ TARGET="/usr/lib/u-boot/$BOARD" u-boot-install-sunxi64 -f /tmp/bootsector.bin
 # Extract full u-boot binary
 dd if=/tmp/bootsector.bin of=/boot/u-boot-sunxi-with-spl.bin bs=8k skip=1
 
-# Rename kernel boot files so they're recognized by u-boot
+# Copy dtb files to /boot so they can be accessed by u-boot
 KERNEL_VERSION=`linux-version list`
 /etc/kernel/postinst.d/zz-sync-dtb $KERNEL_VERSION
 
